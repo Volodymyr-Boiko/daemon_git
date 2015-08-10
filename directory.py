c00ec0ce6a82b06f225dfa4d02790ce85404a761
vboiko <volodymyr.boiko@sethq.com>
2015-08-10 12:12:08 +0300

    little change

--- directory.py
+++ directory.py
@@ -6,7 +6,14 @@ import os.path
 import logging
 
 
-def check_if_dir(path):
+def make_pull_fetch(path):
+    for item in _get_dirs_path(path):
+        if '.git' in os.listdir(item):
+            os.system('git fetch')
+            os.system('git pull')
+
+
+def _check_if_dir(path):
     if os.path.isdir(path):
         return True
     else:
@@ -14,24 +21,18 @@ def check_if_dir(path):
         return 'Insert correct path'
 
 
-def get_list_of_dirs(path):
+def _get_list_of_dirs(path):
     try:
-        if check_if_dir(path):
+        if _check_if_dir(path):
             return os.listdir(path)
     except OSError:
         return []
 
 
-def get_dirs_path(path):
-    return (path + item for item in get_list_of_dirs(path)
+def _get_dirs_path(path):
+    return (path + item for item in _get_list_of_dirs(path)
             if not item.startswith('.'))
 
-
-def make_pull_fetch(path):
-    for item in get_dirs_path(path):
-        if '.git' in os.listdir(item):
-            os.system('git fetch')
-            os.system('git pull')
 
 
 if __name__ == '__main__':
